5306028ae929660b03dbe0ba30c773e5672323f5
dongHyo <kdhyo98@gmail.com>
2022-10-16 23:28:07 +0900

    feat: 코맨트 등록

---
+++ issue-service/src/main/kotlin/com/fastcampus/issueservice/web/CommentController.kt
@@ -0,0 +1,25 @@
+package com.fastcampus.issueservice.web
+
+import com.fastcampus.issueservice.config.AuthUser
+import com.fastcampus.issueservice.model.CommentRequest
+import com.fastcampus.issueservice.model.CommentResponse
+import com.fastcampus.issueservice.service.CommentService
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/v1/issues/{issueId}/comments")
+class CommentController(
+	private val commentService: CommentService
+) {
+
+	@PostMapping
+	fun create(
+		authUser: AuthUser,
+		@PathVariable issueId: Long,
+		@RequestBody request: CommentRequest,
+	): CommentResponse {
+		return commentService.create(issueId, authUser.userId, authUser.username, request)
+	}
+
+
+}
